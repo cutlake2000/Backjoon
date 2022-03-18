2eb522b3fc0441805fbe4e607e4febc22d26fff8
Penguin.Kim <cutlake2000@naver.com>
2022-03-18 18:03:27 +0900

    백준 2839

---
+++ BA_2839.c
@@ -0,0 +1,30 @@
+// 백준 알고리즘 2839번
+// https://www.acmicpc.net/problem/2839
+
+#include <stdio.h>
+
+int main(){
+    int delivery = 0, count = 0;
+    scanf("%d", &delivery);
+    
+    while (1){
+        if (delivery == 0){
+            printf("%d", count);
+            break;
+        }
+        else if (delivery < 3){
+            printf("-1");
+            break;
+        }
+        else{
+            if (delivery % 5 == 0){
+                count += delivery / 5;
+                delivery = 0;
+            }
+            else{
+                delivery -= 3;
+                count++;
+            }
+        }
+    }
+}
