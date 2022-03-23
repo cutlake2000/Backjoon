44d540d7ec25affff3c0726d12d49cb62f3a76d4
Penguin.Kim <cutlake2000@naver.com>
2022-03-23 23:32:47 +0900

    백준 2558

---
+++ BA_2558.c
@@ -0,0 +1,12 @@
+// 백준 알고리즘 2558번
+// "A+B-2"
+// https://www.acmicpc.net/problem/2558
+
+#include <stdio.h>
+
+int main(){
+    int A, B = 0;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    printf("%d", A + B);
+}
