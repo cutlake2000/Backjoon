7d667d2cd122ebdc12285177e2c75b4c7d42c251
Penguin.Kim <cutlake2000@naver.com>
2022-03-28 23:45:01 +0900

    백준 24736

---
+++ BA_24736.c
@@ -0,0 +1,16 @@
+// 백준 24736반
+// 'Football Scoring'
+// https://www.acmicpc.net/problem/24736
+
+#include <stdio.h>
+
+int main(){
+    int visiting_Team[5] = {0,};
+    int home_Team[5] = {0,};
+
+    for (int i = 0 ; i < 5 ; i++) scanf("%d", &visiting_Team[i]);
+    for (int i = 0 ; i < 5 ; i++) scanf("%d", &home_Team[i]);
+
+    printf("%d ", 6 * visiting_Team[0] + 3 * visiting_Team[1] + 2 * visiting_Team[2] + 1 * visiting_Team[3] + 2 * visiting_Team[4]);
+    printf("%d", 6 * home_Team[0] + 3 * home_Team[1] + 2 * home_Team[2] + 1 * home_Team[3] + 2 * home_Team[4]);
+}
