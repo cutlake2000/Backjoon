b1fe4b45c15411eceae597f2670d8d8b4e18709a
Penguin.Kim <cutlake2000@naver.com>
2022-04-01 23:01:18 +0900

    백준 5522

---
+++ BA_5522.c
@@ -0,0 +1,16 @@
+// 백준 5522번
+// '카드 게임'
+// https://www.acmicpc.net/problem/5522
+
+int main(){
+    int temp, result;
+    temp = result = 0;
+
+    for (int i = 0 ; i < 5 ; i++){
+        temp = 0;
+        scanf("%d", &temp);
+        result += temp;
+    }
+
+    printf("%d", result);
+}
