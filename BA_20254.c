312d5b6717fe8a39cb084712a2a7004374901c3d
Penguin.Kim <cutlake2000@naver.com>
2022-04-02 14:10:49 +0900

    백준 20254

---
+++ BA_20254.c
@@ -0,0 +1,13 @@
+// 백준 20254번
+ // 'Site Score'
+ // https://www.acmicpc.net/problem/20254
+
+
+ #include <stdio.h>
+
+ int main(){
+     int a, b, c, d;
+     a = b = c = d = 0;
+     scanf("%d %d %d %d %d", &a, &b, &c, &d);
+     printf("%d", 56 * a + 24 * b + 14 * c + 6 * d);
+ }
