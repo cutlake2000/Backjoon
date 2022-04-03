82d823587f23feef81b5f7ebfb8ff2e021e28759
Penguin.Kim <cutlake2000@naver.com>
2022-04-03 17:28:47 +0900

    백준 1271 (미완료)

---
+++ BA_1271.c
@@ -0,0 +1,33 @@
+// 백준 1271번
+// '엄청난 부자2'
+// https://www.acmicpc.net/problem/1271
+
+#include <stdio.h>
+#include <string.h>
+
+int divine_Num(char* ){
+
+}
+
+int main(){
+    char money[1001] = {0,};
+    char man[1001] = {0,};
+    char count[1001] = {0,};
+    char output[1001] = {0,};
+    int money_Len, man_Len;
+
+    money_Len = strlen(money);
+    man_Len = strlen(man);
+
+    for (int i = 0 ; i < money_Len; i++) scanf("%1c", &money[i]);
+    for (int i = 0 ; i < man_Len ; i++) scanf("%1c", &man[i]);
+    
+    for(int i = 0 ; i < money_Len ; i++){
+        if (money[i] < man[i]){
+            divine_Num()
+        }
+        else{
+
+        }
+    }
+}
