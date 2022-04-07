393777b442de6aafbda6cc1a615b7dcc9a0f0c86
Penguin.Kim <cutlake2000@naver.com>
2022-04-07 19:19:10 +0900

    백준 15680

---
+++ BA_15680.c
@@ -0,0 +1,11 @@
+// 백준 15680번
+// '연세대학교'
+// https://www.acmicpc.net/problem/15680
+
+int main(){
+    int menu = 0;
+    scanf("%d", &menu);
+
+    if (menu == 0) printf("YONSEI");
+    else if (menu == 1) printf("Leading the Way to the Future");
+}
