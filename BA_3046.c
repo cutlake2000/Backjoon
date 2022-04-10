6cce07687c60f4c32d6e766613853d01d0a1d332
Penguin.Kim <71587318+cutlake2000@users.noreply.github.com>
2022-04-10 21:07:04 +0900

    백준 3046

---
+++ BA_3046.c
@@ -0,0 +1,12 @@
+// 백준 3046
+// 'R2'
+// https://www.acmicpc.net/problem/3046
+
+#include<stdio.h>
+
+int main(){
+    int a, avr;
+    scanf ("%d %d", &a, &avr);
+    
+    printf("%d", avr * 2 - a);
+}
